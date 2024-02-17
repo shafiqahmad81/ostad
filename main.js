c6dc9d7c92a2e2171e0bcdd420fc2987c5f71f34
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-02-17 19:13:57 +0600

    update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+let myMap= new Map();
+
+
+myMap.set("key1","Bangladesh")
+myMap.set("key2","Pakistan")
+myMap.set("key3","Bhutan")
+myMap.set("key4","Srilanka")
+
+for (let myValue of myMap.keys()){
+  
+console.log(myValue)
+}
+
+01843111877
